d3ad0f58bc1ce419bbd88d4c8ccf064734df5ed5
Martin Raquion <raquionearl@gmail.com>
2019-06-19 16:32:36 +0800

    Finished Stage 1-6

---
+++ build/precache-manifest.42ad4ea668739c43ac1aedfb8774f21a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01e545898d2d4ff35d52ca11d9b4c7ae",
+    "url": "/react-3/index.html"
+  },
+  {
+    "revision": "81f5d80e619d09860d26",
+    "url": "/react-3/static/css/main.1cff5570.chunk.css"
+  },
+  {
+    "revision": "1fa53693a4183b95f34a",
+    "url": "/react-3/static/js/2.8d099010.chunk.js"
+  },
+  {
+    "revision": "81f5d80e619d09860d26",
+    "url": "/react-3/static/js/main.8afa5a8e.chunk.js"
+  },
+  {
+    "revision": "5d8b170a02a79479fa1c",
+    "url": "/react-3/static/js/runtime~main.ac3ef401.js"
+  }
+]);
